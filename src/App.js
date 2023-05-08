b546af344a4b635517dd983d3fc038bbf61c5819
QuanIT01 <n.h.quan126@gmail.com>
2023-05-08 15:24:36 +0700

    basic component testing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Learn testing</h1>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+      <button type="button">Submit</button>
+    </div>
+  );
+}
+
+export default App;
